9c775ba564bfd450ced57e697c6a5e80873c24e7
Endeyr <aaron.thompson42594@gmail.com>
2022-07-08 14:44:18 -0400

    Finish user edit, update, index, and destroy actions

---
+++ .history/db/schema_20220708134300.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_152434) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+  end
+end
