cc2041f69d6cadcea32ce1ff7ed448b943704c2d
Endeyr <aaron.thompson42594@gmail.com>
2022-07-08 10:17:43 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-  get 'users/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
