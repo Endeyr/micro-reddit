bdf28bacf0c7a43e5d5bb5b757e7de4a3c303e25
Endeyr <aaron.thompson42594@gmail.com>
2022-07-11 07:01:15 -0400

    fixed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_180247) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_105751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_180247) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
